283969cad3053a97a8e2a6e3877b9a3dd6d3ef1d
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-03-31 09:53:48 +0800

    Initial Commit

---
+++ 2023_Lesson1_S207/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2023_Lesson1_S207
+//
+//  Created by Emmanuel Yee Hong Wei on 31/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
